2ce31802662d840324188ba5226776c3ca9184c6
Jadilson <jadilsonigor@gmail.com>
2024-12-05 11:37:14 -0300

    :sparkles: cloudinary provider and new relations

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,20 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "strapi-provider-upload-supabase",
+      provider: "cloudinary",
       providerOptions: {
-        apiUrl: env("SUPABASE_API_URL"),
-        apiKey: env("SUPABASE_API_KEY"),
-        bucket: env("SUPABASE_BUCKET"),
-        directory: env("SUPABASE_DIRECTORY"),
-        options: {},
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
+      // provider: "strapi-provider-upload-supabase",
+      // providerOptions: {
+      //   apiUrl: env("SUPABASE_API_URL"),
+      //   apiKey: env("SUPABASE_API_KEY"),
+      //   bucket: env("SUPABASE_BUCKET"),
+      //   directory: env("SUPABASE_DIRECTORY"),
+      //   options: {},
+      // },
       actionOptions: {
         upload: {},
         uploadStream: {},
